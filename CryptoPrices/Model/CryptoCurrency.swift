df00e7f0d78877ea8ab2ad61da431a04cdc11efa
ergeakova <ergeak@gmail.com>
2022-07-04 00:04:35 +0300

    added CryptoCurrency Model

---
+++ CryptoPrices/Model/CryptoCurrency.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoCurrency.swift
+//  CryptoPrices
+//
+//  Created by Erge Gevher Akova on 3.07.2022.
+//
+
+import Foundation
+
+struct CryptoCurrency: Decodable {
+    let currency: String
+    let price: String
+}
